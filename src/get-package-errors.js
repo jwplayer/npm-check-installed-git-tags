c37e710299793e5610b924b01aa9f4b4aac461a0
jwbrandon <bcasey@jwplayer.com>
2023-10-30 15:11:36 -0400

    fix: only consider semver tags

--- src/get-package-errors.js
+++ src/get-package-errors.js
@@ -1,6 +1,7 @@
 const npa = require('npm-package-arg');
 const readRootPkgJson = require('./read-root-pkg-json');
 const fs = require('fs');
+const semverValid = require('semver/functions/valid');
 
 const getPackageErrors = function(cwd) {
   const pkgJson = readRootPkgJson(cwd);
@@ -20,13 +21,17 @@ const getPackageErrors = function(cwd) {
     const installedVersion = JSON.parse(fs.readFileSync(installPath)).version;
 
     // not a git tag version
-    if (!result.gitCommittish || result.gitCommittish.indexOf('v') === -1) {
+    if (!result.gitCommittish || result.gitCommittish.indexOf('v') !== 0) {
       continue;
     }
 
     const expectedVersion = result.gitCommittish
       .replace('semver:', '')
       .replace('v', '');
+
+    if (!semverValid(expectedVersion)) {
+      continue;
+    }
 
     // if versions match than it is the correct git version
     if (installedVersion === expectedVersion) {
